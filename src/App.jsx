73ae0cde2ac0b8c8d48953ccdfab2a7f8aa3c857
Wikan-dev <oneshootindo@gmail.com>
2025-07-03 10:13:36 +0800

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import mb from './assets/menu_button.png';
 import image5 from './assets/image 5.png';
 import image2 from './assets/image 6.png';
 import { useState, useEffect, useRef } from 'react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Login from './login';
+import { useMediaQuery } from 'react-responsive';
 
 const Card = ({ image , title , location }) => {
     const [isExpanded, setIsExpanded] = useState(true);
     const imageRef = useRef(null);
+    const smallDevice = useMediaQuery({ maxWidth: 430 });
+    const mediumDevice = useMediaQuery({ minWidth: 400 });
 
     const toggleImage = () => {
       setIsExpanded(false);
@@ -26,16 +31,24 @@ const Card = ({ image , title , location }) => {
     //   setIsExpanded(!isExpanded);
     // }
 
+    let rightResponsive = '';
+    let textResponsive = '';
+    if (!isExpanded) {
+      if (smallDevice) {rightResponsive = '30px'; 
+        textResponsive = "34px";}
+       else if (mediumDevice) rightResponsive = '350px';
+    }
+
     return (
         <div className='block mr-full bg-black mx-7 relative rounded-[15px] my-10 transition-all transition-2000 hover:scale-101' onClick={toggleImage} ref={imageRef}>
-            <img src={image} alt="gambar" className='w-[228px] h-[127px] lg:w-[452.5px] lg:h-[251.28px] -z-1 rounded-l-[15px] object-cover' style={{ height: !isExpanded ? '500px' : '', transition: "height 1s ease"}}/>
-            <div className='w-[136px] h-[127px] lg:w-[261.87px] lg:h-[251.28px] absolute top-0 left-23 lg:left-48 bg-linear-90 from-[rgba(0, 0, 0, 0)] to-black' style={{ height: !isExpanded ? '500px' : '', transition: "height 1s ease"}}></div>
-            <h1 className="text-white right-10 top-7 lg:top-15 absolute font-bold text-[43px] lg:text-[70px]" style={{right : !isExpanded ? "370px" : "",top : !isExpanded ? "3px" : "", transition : "all 1s ease"}}>{title}</h1>
+            <img src={image} alt="gambar" className='w-[135px] h-[127px] lg:w-[452.5px] lg:h-[251.28px] -z-1 rounded-l-[15px] object-cover' style={{ height: !isExpanded ? '500px' : '', transition: "height 1s ease"}}/>
+            <div className='w-[46px] h-[127px] lg:w-[261.87px] lg:h-[251.28px] absolute top-0 left-23 lg:left-48 bg-linear-90 from-[rgba(0, 0, 0, 0)] to-black' style={{ height: !isExpanded ? '500px' : '', transition: "height 1s ease"}}></div>
+            <h1 className="text-white right-10 md:right-20 top-10 lg:top-15 absolute font-bold text-[33px] lg:text-[70px]" style={{right : rightResponsive,top : !isExpanded ? "3px" : "", transition : "all 1s ease", fontSize : textResponsive}}>{title}</h1>
 
-            <div > 
-              <input type="text" className='bg-black border border-white w-100 h-6 absolute top-30 left-70 lg:left-130 rounded-sm text-white pl-1' style={{opacity : isExpanded ? "0%" : '', transition : "1s ease"}} />
-              <input type="text" className='bg-black border border-white w-100 h-6 absolute top-40 left-70 lg:left-130 rounded-sm text-white pl-1' style={{opacity : isExpanded ? "0%" : '', transition : "1s ease"}} />
-              <button type="text" className='bg-white border border-white w-100 h-8 absolute top-50 left-70 lg:left-130 rounded-sm pl-1 text-black' style={{opacity : isExpanded ? "0%" : '', transition : "1s ease"}}>search using google maps</button>
+            <div className='absolute -top-10 lg:top-0'> 
+              <input type="text" className='bg-black border border-white w-40 lg:w-100 h-6 absolute top-30 left-40 lg:left-70 lg:left-130 rounded-sm text-white pl-1' style={{opacity : isExpanded ? "0%" : '', transition : "1s ease"}} />
+              <input type="text" className='bg-black border border-white w-40 lg:w-100 h-6 absolute top-40 left-40 lg:left-70 lg:left-130 rounded-sm text-white pl-1' style={{opacity : isExpanded ? "0%" : '', transition : "1s ease"}} />
+              <button type="text" className='bg-white border border-white w-40 lg:w-100 h-8 absolute top-50 left-40 lg:left-70 lg:left-130 rounded-sm pl-1 text-black text-[12px]' style={{opacity : isExpanded ? "0%" : '', transition : "1s ease"}}>search using google maps</button>
             </div>
 
             {isExpanded ? 'kecilkan gambar' : 'besarkan gambar'};
@@ -43,8 +56,8 @@ const Card = ({ image , title , location }) => {
     )
 }
 
-const App = () => {
-  const [showMenu, setShowMenu] = useState(true);
+const Home = () => {
+  const [showMenu, setShowMenu] = useState(false);
   const buttonRef = useRef(null);
   const menu = useRef(null);
 
@@ -73,20 +86,12 @@ const App = () => {
         document.removeEventListener("mousedown", outsideClick);
       };}, []);
 
-  // function menuToggle() {
-  //   if (menu.style.display === "hidden") {
-  //     menu.style.display = "block";
-  //   } else {
-  //     menu.style.display === "hidden";
-  //   }
-  // }
-
   return (
     <div>
       <img src={logo} alt="logo" className="absolute min-w-[421.98px] min-h-[457px] max-h-[765.98px] max-h-[741px] right-0 object-cover opacity-[58%] -z-1"/>
         <div className='m-7'>
           <button onClick={menuToggle} ref={buttonRef} className='hover:scale-120 transition-all duration-200'><img src={mb} alt="menu" /></button>
-          <h1 className='w-[227px] text-[93px] font-bold'>
+          <h1 className='w-[227px] text-[53px] lg:text-[96px] font-bold'>
             BEST COSPLAY EVENT
           </h1>
           <h3 className='w-[269px] text-[23px] font-md xl-w-[403px]'>
@@ -97,12 +102,21 @@ const App = () => {
 
       <Card image={image5} title="All Event" />
       <Card image={image2} title="Comunity" />  
-      <div className='pt-10 h-full bg-gray-200 absolute top-0 overflow-hidden transition-all duration-500' ref={menu} style={{width : showMenu ? "200px" : "0px", paddingLeft : showMenu ? "20px" : "0px"}}>
-        <h1 className='text-left font-bold hover:opacity-50 cursor-pointer' >
-          login
-        </h1>
+      <div className='pt-10 h-full bg-gray-200 absolute top-0 overflow-hidden transition-all duration-500' ref={menu} style={{width : !showMenu ? "0px" : "200px", paddingLeft : !showMenu ? "0px" : "20px"}}>
+        <Link to='/login'><h1 className='font-bold'>Login</h1></Link>
       </div>      
     </div>
+  )
+}
+
+const App = () => {
+  return (
+    <BrowserRouter basename='/cosplayEventWeb'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/login.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+const Login = () => {
+    return (
+        <div>
+            <Link to="/login">enter</Link>
+        </div>
+    )
+}
+
+export default Login;
